c6366f26ab9d8079d5719883e719a9635cd766af
levoskaa <laurinyecz.levi@gmail.com>
2020-03-27 15:51:33 +0100

    Feladat 3

---
+++ W9HL9H/W9HL9H.UWP/Renderers/UwpEditorRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using W9HL9H.Renderers;
+using W9HL9H.UWP.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(CustomEditor), typeof(UwpEditorRenderer))]
+namespace W9HL9H.UWP.Renderers
+{
+    public class UwpEditorRenderer : EditorRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.BorderThickness = new Windows.UI.Xaml.Thickness(1);
+            }
+        }
+    }
+}
---
+++ W9HL9H/W9HL9H/Renderers/CustomEditor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace W9HL9H.Renderers
+{
+    public class CustomEditor : Editor
+    { }
+}
