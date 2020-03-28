a6d00d752ddd36482f41a673e07bc6946f8d90bc
levoskaa <laurinyecz.levi@gmail.com>
2020-03-28 12:34:49 +0100

    Feladat 2

--- game-board.ts
+++ game-board.ts
@@ -46,5 +46,65 @@ export class GameBoard {
             this.initializeBoard(this.tableElement, this.board = []);
             this.currentPlayer = this.winner === this.playerOne ? this.playerTwo : this.playerOne;
         }
+        this.registerHandlers(this.board);
+    }
+
+    onTileClicked(tile: Tile) {
+        if (tile.state === TileState.Empty && this.winner === undefined) {
+            if (this.currentPlayer === this.playerOne) {
+                tile.setState(TileState.X);
+                this.currentPlayer = this.playerTwo;
+            } else if (this.currentPlayer === this.playerTwo) {
+                tile.setState(TileState.O);
+                this.currentPlayer = this.playerOne;
+            }
+            this.checkWinner();
+        }
+    }
+
+    checkWinner() {
+        var points = 0;
+        for (let fun of [
+            (i: number, j: number) => this.board[i][j],
+            (i: number, j: number) => this.board[j][i]
+            ]) {
+            for (let i = 0; i < this.x; i++) {
+                let state = TileState.Empty;
+                points = 1;
+                for (let j = 0; j < this.y; j++) {
+                    let tile = fun(i, j);
+                    console.log(`${i}, ${j}, ${tile.state}, ${state}, ${points}`);
+                    if (tile.state !== TileState.Empty && tile.state == state) {
+                        if (++points >= 5) {
+                            this.won(tile.state === TileState.X ? this.playerOne : this.playerTwo);
+                        }
+                    } else {
+                        points = 1;
+                    }
+                    state = tile.state;
+                }
+            }
+        }
+    }
+
+    won(player: Player) {
+        alert("Player " + player.id + " won! Congrats, " + player.name + "!");
+        player.gamesWon++;
+        var continueButton = $(".continue-game");
+        continueButton.removeAttr("disabled").click(() => {
+            continueButton.attr("disabled", "disabled");
+            this.winner = undefined;
+            this.startGame();
+        });
+        this.winner = player;
+    }
+
+    registerHandlers(board: Tile[][]) {
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                let tile = board[i][j];
+                tile.element.click(() => this.onTileClicked(tile));
+            }
+        }
     }
 }
