928fa45a6879a013c4490dbe4dc3cb9cc7298eb9
levoskaa <laurinyecz.levi@gmail.com>
2020-03-29 13:57:30 +0200

    Feladat 1

--- app/components/business-card-app/business-card-app.component.ts
+++ app/components/business-card-app/business-card-app.component.ts
@@ -11,12 +11,13 @@ import { Card } from "../../models/card.type";
 })
 export class BusinessCardAppComponent implements OnInit {
     constructor(private router: Router) { }
+    
     ngOnInit() {
         this.currentPageTitle = this.router.events
             .filter(e => e instanceof NavigationEnd)
             .map((() => _.find(["Cards", "Companies"], t => this.router.isActive('/' + t.toLowerCase(), false))).bind(this))
     }
-    
+
     title = "Business Card Manager";
     isNavbarCollapsed = true;
     currentPageTitle: Observable<string>;
--- app/components/card-page/card-page.component.ts
+++ app/components/card-page/card-page.component.ts
@@ -11,6 +11,24 @@ import * as _ from "lodash";
     selector: "card-page",
     templateUrl: "./card-page.component.html"
 })
-export class CardPageComponent {
+export class CardPageComponent implements OnInit {
+    constructor(private cardService: CardService, private companyService: CompanyService) { }
+
+    ngOnInit() {
+        this.getCards();
+        this.companyService.getCompanies().subscribe(cmp => this.companies = cmp);
+    }
+
+    getCards() {
+        this.cards = this.cardService.getCards();        
+    }
     
+    getCompanyName(id: number) {
+        let company = _.find(this.companies || [], c => c.id == id);
+        return company && company.name;
+    }
+
+    companies: Company[];
+    selectedCard: Card;
+    cards: Observable<SearchResult<Card>>;
 }
